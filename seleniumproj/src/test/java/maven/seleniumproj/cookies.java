f04738264229c1ff1d6c730c65fce4836dd5d607
GachhiDe <GachhiDe@192.168.0.6>
2026-01-20 11:46:01 +0530

    commit

---
+++ seleniumproj/src/test/java/maven/seleniumproj/cookies.java
@@ -0,0 +1,24 @@
+package maven.seleniumproj;
+
+import org.openqa.selenium.Cookie;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class cookies {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		WebDriver driver;
+		driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com/");
+		Thread.sleep(3000);
+		System.out.println(driver.manage().getCookies());
+		driver.manage().addCookie(new Cookie("Anil","Deepak"));
+		System.out.println(driver.manage().getCookies());
+		driver.manage().deleteAllCookies();
+		System.out.println(driver.manage().getCookies());
+		
+	}
+
+}
